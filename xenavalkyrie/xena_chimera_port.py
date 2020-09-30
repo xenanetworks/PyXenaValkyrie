7caf27205cc02cbd2d9ed056ebc37da51bfc197d
Michael da Costa Carneiro <mcc@xenanetworks.com>
2020-09-30 14:31:14 +0200

    Added Chimera specific methods to the 'new' Chimera port object.

--- xenavalkyrie/xena_chimera_port.py
+++ xenavalkyrie/xena_chimera_port.py
@@ -12,3 +12,33 @@ from xenavalkyrie.xena_port import XenaBasePort, XenaPort
 class XenaChimeraPort(XenaBasePort):
     def __init__(self, parent, index):
         super(XenaChimeraPort, self).__init__(parent=parent, index=index)
+
+    def start_emulate(self):
+        """ Start emulation on port.
+
+        """
+        self.send_command('p_emulate', 'on')
+
+    def stop_emulate(self):
+        """ Stop emulation on port.
+
+        """
+        self.send_command('p_emulate', 'off')
+
+    def enable_delay(self, fid):
+        """ Stop emulation on port.
+
+        """
+        self.send_command('ped_enable[{},2]' . format(fid), 'on')
+
+    def disable_delay(self, fid):
+        """ Turn off delay / jitter on flow.
+
+        """
+        self.send_command('ped_enable[{},2]' . format(fid), 'off')
+
+    def set_const_delay(self, fid, const_delay_ns):
+        """ Stop emulation on port.
+
+        """
+        self.send_command('ped_const[{},2]' . format(fid), '{}' . format(const_delay_ns))
