5c6fa2c58785a7cda7ec33216d919f50fa082a86
Juan Rios <jrr@xenanetworks.com>
2023-07-21 15:45:28 +0200

    Fixed indent

--- xenavalkyrie/xena_app.py
+++ xenavalkyrie/xena_app.py
@@ -489,7 +489,7 @@ class XenaChassis(XenaObject):
         #for location in locations:
         #    ip, module, port = location.split('/')
                 #self.send_command([f'{module}/{port}'], 'p_traffic', command)
-            self.send_command(f'{module}/{port}', 'p_traffic', command)
+                self.send_command(f'{module}/{port}', 'p_traffic', command)
         
         #self.send_command('3/0', 'p_traffic', command)
         #self.send_command('3/1', 'p_traffic', command)
